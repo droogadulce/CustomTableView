6e6a90c44e5da6175d8a136ac0ad632b40fdd8a4
Mayra Aceves <ahigareda.mayra@ciencias.unam.mx>
2019-09-19 16:39:16 -0500

    Initial Commit

---
+++ CustomTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableView
+//
+//  Created by Laboratorio UNAM-Apple 06 on 9/19/19.
+//  Copyright © 2019 UNAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
