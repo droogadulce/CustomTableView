fe071a6f3774c1e4427dcb55dc83ee819d020828
Mayra Aceves <ahigareda.mayra@ciencias.unam.mx>
2019-09-19 17:26:14 -0500

    ADD: numberOfRows and CellForRow modified

---
+++ CustomTableView/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  CustomTableView
+//
+//  Created by Laboratorio UNAM-Apple 06 on 9/19/19.
+//  Copyright © 2019 UNAM. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CustomTableView/ViewController.swift
+++ CustomTableView/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let stringArray = [String]()
 
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var itemList: UITableView!
+    @IBOutlet weak var buttonAdd: UIButton!
+    
+    @IBAction func addPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stringArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ItemTableViewCell", for: indexPath) as? ItemTableViewCell else {return UITableViewCell()}
+        return cell
+    }
+    
+    
+    
 }
 
